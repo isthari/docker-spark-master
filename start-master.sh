19f44a846486cf2ccc3726abc48c68561d045c3a
jhernan <jose.hernandez@isthari.com>
2016-05-03 23:38:54 +0200

    initial commit

---
+++ start-master.sh
@@ -0,0 +1,6 @@
+                                                                                                                          
+/root/spark/bin/spark-class org.apache.spark.deploy.master.Master 
+
+#/root/spark/sbin/start-master.sh -i 0.0.0.0 
+#tail -f /root/spark/logs/*master.out
+#/bin/bash
