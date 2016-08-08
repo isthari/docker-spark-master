a3d6fccde3eff0b6b58207936ec985e684ab3d5d
jhernan <jose.hernandez@isthari.com>
2016-08-08 17:27:02 +0200

    a

--- start-master.sh
+++ start-master.sh
@@ -4,7 +4,8 @@ SPARK_MASTER_HOST=0.0.0.0
 SPARK_MASTER_WEBUI_PORT=7077
 
 echo "Using address $IP"                                                                      
-/root/spark/bin/spark-class org.apache.spark.deploy.master.Master -h $IP --port 7077 --webui-port 8080
+echo "hostname $(hostname)"
+/root/spark/bin/spark-class org.apache.spark.deploy.master.Master -h 0.0.0.0 --port 7077 --webui-port 8080
 
 #/root/spark/sbin/start-master.sh -i 0.0.0.0 
 #tail -f /root/spark/logs/*master.out
