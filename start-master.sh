c98f15e9a5a6fff4765df818e436c4dff22ef633
jhernan <jose.hernandez@isthari.com>
2016-08-08 17:18:33 +0200

    fixing start script

--- start-master.sh
+++ start-master.sh
@@ -1,6 +1,10 @@
 IP=$(hostname -i)                                                   
+SPARK_MASTER_PORT=8080
+SPARK_MASTER_HOST=0.0.0.0
+SPARK_MASTER_WEBUI_PORT=7077
+
 echo "Using address $IP"                                                                      
-/root/spark/bin/spark-class org.apache.spark.deploy.master.Master -h $IP
+/root/spark/bin/spark-class org.apache.spark.deploy.master.Master -h $IP --port 7077 --webui-port 8080
 
 #/root/spark/sbin/start-master.sh -i 0.0.0.0 
 #tail -f /root/spark/logs/*master.out
