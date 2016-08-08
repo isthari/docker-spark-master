512925e56f0b57f8e910790f9ea4be2becc7b6dc
jhernan <jose.hernandez@isthari.com>
2016-08-08 14:49:42 +0200

    issue #0002704: Problema en arranque de spark cuando tiene asociado un servicio

--- start-master.sh
+++ start-master.sh
@@ -1,5 +1,5 @@
                                                                                                                           
-/root/spark/bin/spark-class org.apache.spark.deploy.master.Master 
+/root/spark/bin/spark-class org.apache.spark.deploy.master.Master -i 0.0.0.0 
 
 #/root/spark/sbin/start-master.sh -i 0.0.0.0 
 #tail -f /root/spark/logs/*master.out
